10044e4ec2957799805ab6d4f5059dab300c6d5a
noah.liu <haoling.liu@plejd.com>
2023-03-19 23:38:09 +0100

    stderr info
    update the inf of stderr

--- chop.sh
+++ chop.sh
@@ -10,7 +10,7 @@ index="$2"
 
 # Check if the string is empty or if the index is out of range
 if [ -z "$string" ] || [ "$index" -lt 0 ] || [ "$index" -ge "${#string}" ]; then
-  echo "Error: you must be input as chop.sh  <string> <index>." >&2
+  echo "Error: you must be input appropriate string and index " >&2
   exit 1
 fi
 
