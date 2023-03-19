ae06f8acea7f23eee2e65bdf9f10e0daeff09633
something <something.something>
2023-03-19 22:14:40 +0100

    first commit

--- chop.sh
+++ chop.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/bin/bash https://github.com/su2700/bash.git
+if [ "$#" lt 2 ] ; then
+    echo "Usage: `chop me <sting> <index>"
+    exit 1
+fi
+
+if [ "$1" == "-h" ] ; then
+    echo "Usage: `basename $0` [-h]"
+    exit 0
+fi
 
 # ${stringA:n:m}  means chop from stringA 
 # start from index n and chop m charact
@@ -9,8 +18,6 @@ index="$2"
 chopped="${stringA:0:index}"
 # echo result
 echo "$chopped"
-
-
 
 
 
