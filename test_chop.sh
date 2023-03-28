28f1bd9d99d0ef682fffef004b8b4f7bb676b347
noah.liu <haoling.liu@plejd.com>
2023-03-28 08:29:24 +0200

    test input argument from terminal

--- test_chop.sh
+++ test_chop.sh
@@ -3,8 +3,8 @@
 
 _funcChop()
 {
-    local string="$1"
-    local index="$2"
+     string="$1"
+     index="$2"
 
 if [[ -z "${string}" ]] || [[ "${index}" -lt 0 ]] || [[ "${index}" -ge "${#string}" ]]; then
   echo "false: you must be input appropriate string and index " >&2
@@ -14,10 +14,5 @@ fi
     echo "${chopped}"
 }
 
-
-# Check if the first argument is a string
-
-
-
 # Call the funcation 
-_funcChop plejdland 20
+_funcChop plejdland 7
