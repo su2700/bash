b04692a18bb71bea947d5dcd60260326ac4d9480
noah.liu <haoling.liu@plejd.com>
2023-03-29 13:14:41 +0200

    func the test process

--- test_chop.sh
+++ test_chop.sh
@@ -3,7 +3,37 @@
 # this test script will test 5 kinds of cases of user input for the script chop.sh
 # if 1, zero string, 2, negative index, 3, no index, 4, no arguments, 5, index > length
 
-# test case1
+function _test_case()
+{
+  parameter1=$1
+  parameter2=$2
+  #expected_ret=$3
+  expected_output=$3
+
+# call UUT
+actual_output=$(./chop.sh "${parameter1}"  "${parameter2}")
+
+if [[ "${expected_output}" != "${actual_output}" ]]; then
+  echo "test failed"
+  else
+  echo "test passed"
+fi
+}
+
+_test_case "numbers" 2 "nu" 
+
+_test_case "numbers" -2 "nu" 
+
+_test_case "numbers" 30 
+
+_test_case "numbers" 2 
+
+
+
+
+<<com
+
+test case1
 echo " "
 echo "case no 1"
 expected_output="plejd"
@@ -44,3 +74,4 @@ if [[ "${actual_output}" == "${expected_output}" ]]; then
 else
   echo "Test case 4 failed: index should not big than length of string"  
 fi
+com
