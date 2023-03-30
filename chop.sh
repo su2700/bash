c42f9a9fa2b422919781593fd270401d54ce2b62
noah.liu <haoling.liu@plejd.com>
2023-03-30 07:59:22 +0200

    update the all the test case and exit vaule for chop.sh

--- chop.sh
+++ chop.sh
@@ -11,28 +11,25 @@ _funcChop()
 }
 
 
-if [[ "$1" = "-h" ]] || [[ "$#" -eq 0 ]] || [[ "$1" = "--help" ]]; then
-  echo "Usage: chop_me <string> <index>"
-  echo "this Chops script the given string at the specified index, then prints the result."
+if  [[ "$1" = "-h" ]] || [[ "$#" -eq 0 ]] || [[ "$1" = "--help" ]]; then
+  echo "this Chops script the given string at the specified index, then prints the result. Usage: chop_me <string> <index>"
   exit 1
 fi
 
-if [[ "$#" -lt 2 ]]; then
-  echo "Usage: chop <string> <index>" >&2
-  exit 1
-fi
 # start from index n and chop m charact
 string="$1"
 index="$2"
 
 
 # Check if the string is empty or if the index is out of range
-if [[ -z "$string" ]] || [[ "$index" -lt 0 ]] || [[ "$index" -ge "${#string}" ]]; then
+if [[ "$#" -lt 2 ]]||[[ -z "$string" ]] || [[ "$index" -lt 0 ]] || [[ "$index" -ge "${#string}" ]]; then
   echo "Error: you must be input appropriate string and index " >&2
-  exit 1
+  exit 2
 fi
 
 #call function
 chopResult=$(_funcChop "$1" "$2")
 
 echo "${chopResult}"
+
+exit 0
--- test_chop.sh
+++ test_chop.sh
@@ -5,20 +5,20 @@
 
 function _test_case()
 {
-  parameter1=$1
-  parameter2=$2
-  #expected_ret=$3
-  expected_output=$3
+    p1=$1
+    p2=$2
+    expected_ret=$3
+    expected_output=$4
 
-# call UUT
-# need check for the script chop.sh exist in the right path and executable, how? 
-actual_output=$(./chop.sh "${parameter1}"  "${parameter2}")
+    # call UUT
+    # need check for the script chop.sh exist in the right path and executable, how? 
+    actual_ret=$(./chop.sh "${p1}"  "${p2}")
 
-if [[ "${expected_output}" != "${actual_output}" ]]; then
-  echo "test failed"
-  else
-  echo "test passed"
-fi
+    if [[ "${expected_ret}" != "${actual_ret}" ]]; then
+    echo "test failed"
+    else
+    echo "test passed"
+    fi
 }
 
 # how many cases we many have to test total,  
@@ -29,61 +29,16 @@ fi
 
 
 # 1st case test, 
-_test_case "numbers" 2 "nu" 
+_test_case "numbers" 2 "nu" 0
 
-# 2nd case test, inputs string and negative index values, expect get error back
-_test_case "numbers" -2 "nu" 
+# 2nd case test, inputs string and negative index values, expect returen "" , exp output 1
+_test_case "numbers" -2 "Error: you must be input appropriate string and index " 2
+_test_case "numbers" 30 "Error: you must be input appropriate string and index " 2
 
 # 3rd case test, inputs string and no index values, expect get error back
-_test_case "numbers" 
-
-#4th case test, inputs string and  index values bigger than length of string, expect get error back
-_test_case "numbers" 30
-
-
+_test_case "numbers" "" "Usage: chop <string> <index>" 1 
+_test_case "numbers" "-h" "Usage: chop <string> <index>" 1 
+_test_case "numbers" "--help" "Usage: chop <string> <index>" 1 
+_test_case "" "" "Usage: chop <string> <index>" 1 
 
 
-<<com
-
-test case1
-echo " "
-echo "case no 1"
-expected_output="plejd"
-
-actual_output=$(./chop.sh )
-if [[ "${actual_output}" == "${expected_output}" ]]; then
-  echo "Test case 1 passed"
-else
-  echo "Test case 1 failed: error, no string"  
-fi
-
-echo " "
-# test case2
-echo "case no 2"
-actual_output=$(./chop.sh plejdland -2)
-if [[ "${actual_output}" == "${expected_output}" ]]; then
-  echo "Test case 4 passed"
-else
-  echo "Test case 4 failed: negative index"  
-fi
-
-echo " "
-# test case3
-echo "case no 3"
-actual_output=$(./chop.sh plejdland )
-if [[ "${actual_output}" == "${expected_output}" ]]; then
-  echo "Test case 3 passed"
-else
-  echo "Test case 3 failed: no index"  
-fi
-
-echo " "
-# test case4
-echo "case no 4"
-actual_output=$(./chop.sh plejdland 20)
-if [[ "${actual_output}" == "${expected_output}" ]]; then
-  echo "Test case 4 passed"
-else
-  echo "Test case 4 failed: index should not big than length of string"  
-fi
-com
