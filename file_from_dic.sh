e854094d52d3eb78c25d749838fad60f9817040d
noah.liu <haoling.liu@plejd.com>
2023-04-26 08:12:54 +0200

    add file_from_dic.sh

---
+++ file_from_dic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# what is basename: basename used to extract the filename or directory name from path
+# I feel basename is useless:)
+
+for localFileList in *; do
+    echo $(basename "$localFileList")
+done
+
